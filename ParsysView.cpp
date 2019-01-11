f8e107d82c0372549573d6eec8958847635d93e6
Acsor <nildexo@yandex.com>
2019-01-11 23:04:00 +0100

    Perform BIG changes to the `ParsysView' module...
    Changes include:
    	1. Mini-map feature in top-left corner
    	2. Centered moved particle in main view
    	3. Zoom APIs
    	4. `position()', `size()' and similar APIs
    	5. Utility private function `updateViews()'

--- ParsysView.cpp
+++ ParsysView.cpp
@@ -14,15 +14,15 @@ static void particleDecoration(
 );
 
 
-static const Color COLOR_MOVING = Color::Cyan;
-static const Color COLOR_GT = Color::Red;
-static const Color COLOR_LT = Color::Blue;
+void ParsysView::updateViews() {
+	mMainView.setSize(mSize);
+	mMapView.setCenter(mPosition.x + mSize.x / 2, mPosition.y + mSize.y / 2);
+	mMapView.setSize(mSize);
 
-ParsysView::ParsysView(Parsys &s, weak_ptr<Particle> const &controlled) {
-	mSys = &s;
-	mSys->attach(this);
-	mSys->notify();
-	mControlled = controlled;
+	mMainBackground.setPosition(mPosition);
+	mMainBackground.setSize(mSize);
+	mMapBackground.setPosition(mPosition);
+	mMapBackground.setSize(mSize);
 }
 
 const Color ParsysView::COLOR_BACKGROUND = Color(0, 22, 107);
@@ -36,44 +36,125 @@ const Color ParsysView::COLOR_GT = Color(202, 0, 44);
 const Color ParsysView::COLOR_OUT_GT = Color(145, 0, 20);
 const Color ParsysView::COLOR_LT = Color(0, 40, 151);
 const Color ParsysView::COLOR_OUT_LT = Color(0, 4, 98);
+
+ParsysView::ParsysView(Parsys &s, weak_ptr<Particle> const &controlled):
+	mSys{s} {
+	mControlled = controlled;
 	mPviews = new unordered_map<Particle, CircleShape>();
+	mSys.attach(this);
+	
+	mMapView.setViewport(FloatRect(0.0f, 0.0f, 0.22f, 0.25f));
+
+	mMainBackground.setFillColor(COLOR_BACKGROUND);
+	mMainBackground.setOutlineThickness(5);
+	mMainBackground.setOutlineColor(COLOR_OUT_BACKGROUND);
+
+	mMapBackground.setFillColor(COLOR_BACKGROUND_MAP);
+	mMapBackground.setOutlineThickness(-10);
+	mMapBackground.setOutlineColor(COLOR_OUT_BACKGROUND_MAP);
+
+	updateViews();
+	mSys.notify();
 }
 
 ParsysView::~ParsysView() {
 	if (mPviews != nullptr)
 		delete mPviews;
-	mSys->detach(this);
+	mSys.detach(this);
 
 	mControlled.reset();
 }
 
 void ParsysView::draw(RenderTarget& target, RenderStates states) const {
+	mMainView.setSize(target.getSize().x, target.getSize().y);
+	mMainView.zoom(pow(ZOOM_IN, mZoom));
+
+	target.setView(mMainView);
+	target.draw(mMainBackground);
+
+	for (auto i = mPviews->cbegin(); i != mPviews->cend(); i++) {
+		target.draw(i->second, states);
+	}
+
+	target.setView(mMapView);
+	target.draw(mMapBackground);
+
 	for (auto i = mPviews->cbegin(); i != mPviews->cend(); i++) {
 		target.draw(i->second, states);
 	}
 }
 
 void ParsysView::onChange(Observable const *o) {
-	shared_ptr<Particle> p;
 	CircleShape c;
 
 	if (typeid(*o) == typeid(Parsys)) {
+		if (!mControlled.expired()) {
+			mShared = mControlled.lock();
+			mMainView.setCenter(
+				mPosition.x + mShared->x(), mPosition.y + mShared->y()
+			);
+			mShared.reset();
+		}
+
+		// TO-DO Optimize the caching of view instances
 		if (mPviews != nullptr)
 			delete mPviews;
 		mPviews = new unordered_map<Particle, CircleShape>();
 
-        for (auto i = mSys->cbegin(); i != mSys->cend(); i++) {
-        	p = *i;
+        for (auto i = mSys.cbegin(); i != mSys.cend(); i++) {
+        	c = CircleShape();
+			// CircleShape (and any *Shape in general) deals with origin-wise
+			// coordinates. In the case of CircleShape, setPosition() doesn't
+			// look at the center coordinate, but at its top-left by default.
+			c.setOrigin((*i)->radius(), (*i)->radius());
+			c.setRadius((*i)->radius());
+        	c.setPosition(mPosition.x + (*i)->x(), mPosition.y + (*i)->y());
+			particleDecoration(c, **i, mControlled);
 
-        	c = CircleShape(p->radius());
-        	c.setPosition(p->x(), p->y());
-        	c.setFillColor(particleColor(*p, mControlled));
-
-        	mPviews->emplace(*p, c);
+        	mPviews->emplace(**i, c);
         }
 	}
 
 }
+
+void ParsysView::position(float x, float y) {
+	mPosition.x = x;
+	mPosition.y = y;
+	updateViews();
+}
+
+Vector2f ParsysView::position() const {
+	return mPosition;
+}
+
+void ParsysView::size(float width, float height) {
+	if (width <= 0 || height <= 0)
+		throw invalid_argument("Width/height can't be <= 0");
+
+	mSize = Vector2f(width, height);
+	updateViews();
+}
+
+Vector2f ParsysView::size() const {
+	return mSize;
+}
+
+void ParsysView::zoomIn() {
+	mZoom++;
+}
+
+void ParsysView::zoomOut() {
+	mZoom--;
+}
+
+void ParsysView::zoomReset() {
+	mZoom = 0;
+}
+
+short int ParsysView::zoom() const {
+	return mZoom;
+}
+
 
 static void particleDecoration(
 	CircleShape &target, Particle const &p, weak_ptr<Particle> &moving
--- ParsysView.hpp
+++ ParsysView.hpp
@@ -12,11 +12,25 @@ using namespace std;
 
 class ParsysView: public Drawable, public Observer {
 	private:
-		Parsys *mSys{nullptr};
-		weak_ptr<Particle> mControlled;
+		Parsys &mSys;
 		unordered_map<Particle, CircleShape> *mPviews{nullptr};
-	
+
+		weak_ptr<Particle> mControlled;
+		// Variable mainly introduced for optimization.
+		shared_ptr<Particle> mShared;
+
+		View mutable mMainView, mMapView;
+		Vector2f mPosition{0, 0};
+		Vector2f mSize{512, 512};
+		short int mZoom{0};
+
+		RectangleShape mMainBackground, mMapBackground;
+
+		void updateViews();
 	public:
+		static constexpr float ZOOM_IN = 10 / 11.0;
+		static constexpr float ZOOM_OUT = 11 / 10.0;
+
 		static const Color COLOR_BACKGROUND;
 		static const Color COLOR_OUT_BACKGROUND;
 		static const Color COLOR_BACKGROUND_MAP;
@@ -34,6 +48,17 @@ class ParsysView: public Drawable, public Observer {
 
 		void draw(RenderTarget& target, RenderStates states) const override;
 		void onChange(Observable const *o) override;
+
+		// TO-DO Shall I use the `set/get' prefix as in the SFML API?
+		void position(float x, float y);
+		Vector2f position() const;
+		void size(float width, float height);
+		Vector2f size() const;
+
+		void zoomIn();
+		void zoomOut();
+		void zoomReset();
+		short int zoom() const;
 };
 
 
